2c6d41e82ace5d22f897d98a61a6b592fd721090
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-08 17:02:04 -0600

    schema.sql - file updated with correct columns and rows

--- schema.sql
+++ schema.sql
@@ -3,15 +3,13 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
--- Create the table tasks.
 CREATE TABLE burgers (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(50) NOT NULL,
-  consumed BOOLEAN NOT NULL DEFAULT 0;
+  consumed BOOLEAN NOT NULL DEFAULT 1,
   PRIMARY KEY (id)
 );
 
--- Insert a set of records.
-INSERT INTO buger (name) VALUES ('California Buger');
-INSERT INTO buger (name) VALUES ('Cheeseburger');
-INSERT INTO buger (name) VALUES ('Bacon & Blue Burger');
+INSERT INTO burgers (name) VALUES ('California Buger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon & Blue Burger');
