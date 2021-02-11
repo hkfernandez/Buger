d6b4aa2825ea4d5fa0e6d383a56dbd4b212d9f5f
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-11 15:31:44 -0600

    uncommenting line by line before pushing to heroku

--- model/burger.js
+++ model/burger.js
@@ -5,12 +5,12 @@ const burger = {
             orm.all("burgers", function(res) {
               cb(res);
             });
-          }
+          },
           create: function(burgerName, cb) {
             orm.create('burgers', 'name', burgerName, function(res) {
               cb(res);
             });
-          },
+          }
           // update: function(burgerId, cb) {
           //   orm.update('burgers', 'consumed', 0, 'id', burgerId, function(res) {
           //     cb(res);
