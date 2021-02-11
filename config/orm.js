96e8d736d245d43c35bf0737bbf75fa167819d30
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-11 15:20:23 -0600

    uncommenting line by line before pushing to heroku

--- config/orm.js
+++ config/orm.js
@@ -11,16 +11,16 @@ var orm = {
 			}
 		);
 	}
-	// create: function(table, col, val, cb) {
-      //       let queryStr = `INSERT INTO ${table} (${col}) VALUES ("${val}");`;
-	// 	console.log(queryStr);
-	// 	connection.query(queryStr,
-	// 		function(err, result) {
-	// 			if (err) throw err;
-	// 			cb(result);
-	// 		}
-	// 	);
-	// },
+	create: function(table, col, val, cb) {
+            let queryStr = `INSERT INTO ${table} (${col}) VALUES ("${val}");`;
+		console.log(queryStr);
+		connection.query(queryStr,
+			function(err, result) {
+				if (err) throw err;
+				cb(result);
+			}
+		);
+	},
 	// update: function(table, col1, val1, col2, val2, cb) {
 	// 	let queryStr = `UPDATE ${table} SET ${col1} = ${val1} WHERE ${col2} = ${val2}`;
 	// 	connection.query( queryStr, 
